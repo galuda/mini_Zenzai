31d50eddba634d5fe799e02146e5c70b44f50dfe
galuda <mkrnytm_sd-90@docomo.ne.jp>
2022-02-21 15:48:55 +0900

    動画視聴ページのリンクのみクリック処理変更

--- mini_Zenzai.user.js
+++ mini_Zenzai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name			mini_Zenzai
 // @author			galuda
-// @version			0.0.1
+// @version			0.0.2
 // @description		ZenzaWatch の劣化版
 // @namespace		https://github.com/galuda/mini_Zenzai
 // @updateURL		https://github.com/galuda/mini_Zenzai/raw/main/mini_Zenzai.user.js
@@ -10,6 +10,20 @@
 // @match			*://www.nicovideo.jp/*
 // ==/UserScript==
 
-(() => {
-	console.log('v0.0.1');
+// プログラムの開始地点
+(function () {
+	// リンクを抽出
+	let links = document.querySelectorAll('a');
+	links.forEach(function (link) {
+		const url_sm = /.*:\/\/www\.nicovideo\.jp\/watch\/sm.*/;
+		let url_check = url_sm.test(link.getAttribute('href'));
+		// 動画視聴ページのみ抽出
+		if (url_check) {
+			link.addEventListener('click', function (e) {
+				// ページを開かない
+				e.preventDefault();
+				console.log(link.getAttribute('href'));
+			});
+		}
+	});
 })();
