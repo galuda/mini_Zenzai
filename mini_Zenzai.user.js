8eb9e6f46da2de447d9a868f55a093b8b8eeee27
galuda <mkrnytm_sd-90@docomo.ne.jp>
2022-02-21 14:12:20 +0900

    first

---
+++ mini_Zenzai.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name			mini_Zenzai
+// @author			galuda
+// @version			0.0.1
+// @description		ZenzaWatch の劣化版
+// @namespace		https://github.com/galuda/mini_Zenzai
+// @match			*://www.nicovideo.jp/*
+// ==/UserScript==
+
+(() => {
+	console.log('v0.0.1');
+})();
